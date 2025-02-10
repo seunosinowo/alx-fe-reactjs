712a3a0a5475705be7b0c3c30516da3090e3bf7f
seunosinowo <oluwaseunpaul98@gmail.com>
2025-02-10 10:25:41 +0100

    User profile added

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,6 +1,7 @@
 import Footer from "./components/Footer"
 import Header from "./components/Header"
 import MainContent from "./components/MainContent"
+import UserProfile from "./components/UserProfile"
 import WelcomeMessage from "./components/WelcomeMessage"
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
     </>
   )
 }
