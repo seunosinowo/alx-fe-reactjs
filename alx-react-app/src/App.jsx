98ba7710bcff5e6c5e4736a50b433546f6ffecb5
seunosinowo <oluwaseunpaul98@gmail.com>
2025-02-17 11:49:16 +0100

    Implemented User Components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,6 +1,8 @@
+import UserContext from "./components/UserContext"
 import Footer from "./components/Footer"
 import Header from "./components/Header"
 import MainContent from "./components/MainContent"
+import ProfilePage from "./components/ProfilePage"
 import UserProfile from "./components/UserProfile"
 import WelcomeMessage from "./components/WelcomeMessage"
 
@@ -14,6 +16,13 @@ function App() {
       <MainContent />
       <Footer />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+
+      <ProfilePage userData={userData}/>
+
+      <UserContext.Provider value={userData}>
+        <ProfilePage />
+      </UserContext.Provider>
+      
     </>
   )
 }
