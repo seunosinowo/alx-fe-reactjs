d6145ec53eb57d1850a986151bbc0d42539d855a
seunosinowo <oluwaseunpaul98@gmail.com>
2025-02-17 11:53:49 +0100

    Implemented User Props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,8 +2,6 @@ import UserContext from "./components/UserContext"
 import Footer from "./components/Footer"
 import Header from "./components/Header"
 import MainContent from "./components/MainContent"
-import ProfilePage from "./components/ProfilePage"
-import UserProfile from "./components/UserProfile"
 import WelcomeMessage from "./components/WelcomeMessage"
 
 function App() {
@@ -16,12 +14,6 @@ function App() {
       <MainContent />
       <Footer />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-
-      <ProfilePage userData={userData}/>
-
-      <UserContext.Provider value={userData}>
-        <ProfilePage />
-      </UserContext.Provider>
       
     </>
   )
