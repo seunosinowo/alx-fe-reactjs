4ec9f3ab3b10350046a8575d0cfc63013df9375f
seunosinowo <oluwaseunpaul98@gmail.com>
2025-02-17 11:39:26 +0100

    Implemented User Components

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -2,11 +2,13 @@ import Counter from "./components/Counter"
 import Footer from "./components/Footer"
 import Header from "./components/Header"
 import MainContent from "./components/MainContent"
+import ProfilePage from "./components/ProfilePage"
+import UserContext from "./components/UserContext"
 import UserProfile from "./components/UserProfile"
 import WelcomeMessage from "./components/WelcomeMessage"
 
 function App() {
-
+  const userData = {name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
     <>
@@ -17,6 +19,11 @@ function App() {
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
 
       <Counter/>
+      <ProfilePage userData={userData}/>
+
+      <UserContext.Provider value={userData}>
+        <ProfilePage />
+      </UserContext.Provider>
     </>
   )
 }
