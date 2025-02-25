13034c2d1184593af33e72d6a07fe1b3fcced2bf
seunosinowo <oluwaseunpaul98@gmail.com>
2025-02-25 11:34:37 +0100

    Resolve repo

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,4 +1,4 @@
-import { useRecipeStore } from './recipeStore';
+import useRecipeStore from './recipeStore';
 
 const DeleteRecipeButton = ({ recipeId }) => {
   const recipe = useRecipeStore(state =>
--- recipe-sharing-app/src/components/RecipeDetails.jsx
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import useRecipeStore from '../store/useRecipeStore';
+import useRecipeStore from './recipeStore';
 
 const RecipeDetails = () => {
   const { name } = useParams();
